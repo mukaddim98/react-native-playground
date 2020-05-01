85cb62f1d88952abfbfee2f17e15dd73695c3ef0
mukaddim98 <mukaddim1998@gmail.com>
2020-05-01 02:39:27 -0300

    Added screens

---
+++ screens/about.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function About() {}
+
+const styles = StyleSheet.create({});
---
+++ screens/home.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Home() {}
+
+const styles = StyleSheet.create({});
